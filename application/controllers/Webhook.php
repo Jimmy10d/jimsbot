0fa7de63382bbdd0460776580f87ecd863bd85e1
Jimmy Chen <jimmy600d@gmail.com>
2017-03-29 20:16:24 +0700

    updated

--- application/controllers/Webhook.php
+++ application/controllers/Webhook.php
@@ -1,40 +1,157 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-use \LINE\LINEBot;
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
-use \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
-use \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+
+
+// SDK for create bot
+
+        use \LINE\LINEBot;
+
+        use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+
+
+
+// SDK for build message
+
+        use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
+        use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+
+        use \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+
+
+
+// SDK for build button and template action
+
+        use \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+
+        use \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+
+
 
 class Webhook extends CI_Controller {
 
-  private $bot;
-  private $events;
-  private $signature; 
-  private $user;
 
-  function __construct()
-  {
-    parent::__construct();
-    $this->load->model('tebakkode_m');
-  }
+    private $events;
 
-  public function index()
-  {
-    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-      echo "Hello Coders!";
-      header('HTTP/1.1 400 Only POST method allowed');
-      exit;
-    } 
+    private $signature;
 
-    // get request
-    $body = file_get_contents('php://input');
-    $this->signature = isset($_SERVER['HTTP_X_LINE_SIGNATURE']) ? $_SERVER['HTTP_X_LINE_SIGNATURE'] : "-";
-    $this->events = json_decode($body, true);
 
-    // log every event requests);
-    $this->tebakkode_m->log_events($this->signature, $body);
-  }
+    private $bot;
+
+    private $user;
+
+
+    function __construct()
+
+    {
+
+        parent::__construct();
+
+        $this->load->model('tebakkode_m');
+
+        // create bot object
+        $httpClient = new CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
+        $this->bot  = new LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
+
+    }
+
+
+    public function index()
+
+    {
+
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+
+            echo "Hello Coders!";
+
+            header('HTTP/1.1 400 Only POST method allowed');
+
+            exit;
+
+        }
+
+
+        // get request
+
+        $body = file_get_contents('php://input');
+
+        $this->signature = isset($_SERVER['HTTP_X_LINE_SIGNATURE'])
+
+                ? $_SERVER['HTTP_X_LINE_SIGNATURE']
+
+                : "-";
+
+        $this->events = json_decode($body, true);
+
+
+        $this->tebakkode_m->log_events($this->signature, $body);
+
+        foreach ($this->events['events'] as $event)
+
+       {
+
+       // skip group and room event
+
+       if(! isset($event['source']['userId'])) continue;
+
+
+
+       // get user data from database
+
+       $this->user = $this->tebakkode_m     ->getUser($event['source']['userId']);
+
+       // respond event
+
+       if($event['type'] == 'message'){
+
+           if(method_exists($this, $event['message']['type'].'Message')){
+
+           $this->{$event['message']['type'].'Message'}($event);
+
+           }
+
+       }
+
+       else {
+
+           if(method_exists($this, $event['type'].'Callback')){
+
+           $this->{$event['type'].'Callback'}($event);
+
+           }
+
+       }
+    }
+
+    }
+
+    private function followCallback($event)
+
+    {
+
+        $res = $this->bot->getProfile($event['source']['userId']);
+
+        if ($res->isSucceeded())
+
+        {
+
+            $profile = $res->getJSONDecodedBody();
+
+
+
+            // save user data
+
+            $this->tebakkode_m->saveUser($profile);
+            // send welcome message
+            $message = "Salam kenal, " . $profile['displayName'] . "!\n";
+            $message .= "Silakan kirim pesan \"MULAI\" untuk memulai kuis.";
+            $textMessageBuilder = new TextMessageBuilder($message);
+            $this->bot->pushMessage($event['source']['userId'], $textMessageBuilder);
+
+        }
+
+    }
+
+
+
 }
--- application/models/Tebakkode_m.php
+++ application/models/Tebakkode_m.php
@@ -1,18 +1,61 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+
 class Tebakkode_m extends CI_Model {
 
-  function __construct(){
-    parent::__construct();
-    $this->load->database();
-  }
 
-  function log_events($signature, $body)
-  {
-    $this->db->set('signature', $signature)
-    ->set('events', $body)
-    ->insert('eventlog');
+    function __construct(){
 
-    return $this->db->insert_id();
-  }
+        parent::__construct();
+
+        $this->load->database();
+
+    }
+
+
+    function log_events($signature, $body)
+
+    {
+
+        $this->db->set('signature', $signature)
+
+        ->set('events', $body)
+
+        ->insert('eventlog');
+
+
+        return $this->db->insert_id();
+
+    }
+
+    function getUser($userId)
+
+    {
+
+        $data = $this->db->where('user_id', $userId)   ->get('users')->row_array();
+
+        if(count($data) > 0)    return $data;
+
+        return false;
+
+    }
+
+
+
+    function saveUser($profile)
+
+    {
+
+        $this->db->set('user_id', $profile['userId'])
+
+        ->set('display_name', $profile['displayName'])
+
+        ->insert('users');
+
+
+
+        return $this->db->insert_id();
+
+    }
+
 }
